c406d767668e0c99e635359448b7092fb49e4996
shah aowal <kashim91@live.com>
2023-11-16 14:53:24 +0000

    initial render

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,84 @@
-import { useState,useEffect } from 'react'
-import stock from './stock.json'
-import './App.css'
-import {nanoid} from 'nanoid'
+import { useState, useEffect } from 'react';
+import stock from './stock.json';
+import './App.css';
+import { nanoid } from 'nanoid';
+
 function App() {
-
-
   const [input, setInput] = useState("");
-  const [stocks,setStocks]=useState(stock)
-  const [stocklist,setStockList]=useState(Object.keys(stocks))
-  const [tableHtml,setTableHtml]=useState([])
-  const [dropDownVisible, setdropDownVisible] = useState(false)
-  const handleChange = ({target:{value}}) => setInput(value)
-  const showDropDown = ()=>setdropDownVisible(true)
-  console.log(stocklist)
+  const [stocks, setStocks] = useState(stock);
+  const [stocklist, setStockList] = useState(Object.keys(stocks));
+  const [tableRows, setTableRows] = useState([]);
+  const [dropDownVisible, setDropDownVisible] = useState(false);
 
-  useEffect(()=>{},[stocks])
+  const handleChange = ({ target: { value } }) => {
+    setInput(value);
+    setDropDownVisible(true); // Show dropdown on input change
+  };
+
+  useEffect(() => {
+    // Filter stocklist based on the input
+    const filteredStockList = stocklist.filter(item =>
+      item.toLowerCase().includes(input.toLowerCase())
+    );
+
+    // Create table rows based on the filtered stocklist
+    const rows = filteredStockList.map(item => {
+      const locations = stocks[item].map((location, index) => (
+        <div key={index}>{location}</div>
+      ));
+
+      return (
+
+
+        <tr key={nanoid()}>
+          <td>{item}</td>
+          <td>{locations}</td>
+        </tr>
+      );
+    });
+
+    setTableRows(rows);
+  }, [input, stocks, stocklist]);
+
   return (
     <div className="App">
-       <div className="flex-col">
+      <div className="flex-col">
+<h1>NMUH Drug Locations</h1>
         <label htmlFor="input">Type a medicine</label>
         <input
-            name="input"
-            type="text"
-            list="stock"
-            value={input}
-            onChange={handleChange}
-            onSelect={()=>{}}
-            
+          name="input"
+          type="text"
+          list="stock"
+          value={input}
+          onChange={handleChange}
         />
       </div>
-      {dropDownVisible && (
+      {!dropDownVisible && (
         <datalist id="stock">
           {stocklist.map((item) => (
-            <option  key={nanoid()}>
-              {item}
-            </option>
+            <option key={nanoid()}>{item}</option>
           ))}
         </datalist>
       )}
-        <table>
+      <table>
+        <thead>
+          <tr>
+            <th>Drug Name</th>
+            <th>Location</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tableRows.length > 0 ? (
+            tableRows
+          ) : (
             <tr>
-              <th>Drug Name</th>
-              <th>Location</th>
+              <td colSpan="2">No matching drugs found</td>
             </tr>
-            {/* 
-            dynamically render a list of rows in the format
-            
-            // step 1 get drug name- filter stocklist
-            
-            // step 2 get drug stock locations 
-            <tr>
-            stock.get(['Drug Name])- will return an array- iterate over array
-            
-            <td>{****DRUG NAME***}</td>
-              <td><****DRUG LOCATION***}</td>
-            </tr>
-            
-            
-            */}
-        </table>
+          )}
+        </tbody>
+      </table>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
